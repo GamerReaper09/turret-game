e5030dcf87ff8c3238290af6a596ff7ab610a6b7
GamerReaper09 <vilmer.hagelberg@gmail.com>
2026-06-24 11:48:44 +0200

    Shop added

--- src/renderer.js
+++ src/renderer.js
@@ -33,7 +33,18 @@ const ctx = canvas.getContext("2d");
 const gameOverDiv = document.getElementById("gameOverDiv");
 const gameOverScore = document.getElementById("gameOverScore");
 const scoreCounter = document.getElementById("score");
-
+const nextWaveCounter = document.getElementById("nextWave");
+const cashCounter = document.getElementById("cashCounter");
+const speedCounter = document.getElementById("speedCounter");
+const cost1Counter = document.getElementById("cost1Counter");
+const bonusCounter = document.getElementById("cashBonusCounter");
+const cost2Counter = document.getElementById("cost2Counter");
+const sizeCounter = document.getElementById("sizeCounter");
+const cost3Counter = document.getElementById("cost3Counter");
+const enemySpeedCounter = document.getElementById("enemySpeedCounter");
+const cost4Counter = document.getElementById("cost4Counter");
+const cost5Counter = document.getElementById("cost5Counter");
+const cost6Counter = document.getElementById("cost6Counter");
 
 const turret = {
   x:960,
@@ -48,8 +59,7 @@ const mouse = {
   yRel:0,
 };
 
-let score,alive,waveSize,waveCd,projectiles,enemies,prevWave,time
-
+let score,alive,waveSize,waveCd,projectiles,enemies,prevWave,time,cash,bulletSpeed,cost1,cost2,cashBonus,bulletSize,cost3,enemySpeed,cost4,cost5,multishot,cost6,secret;
 
 
 function initialize() {
@@ -58,13 +68,36 @@ function initialize() {
   alive = true;
   waveSize = 5;
   waveCd = 5000;
-  time = performance.now()
+  time = performance.now();
   prevWave = time-4000;
-  
+  cash = 0;
+  bulletSpeed = 2;
+  cost1 = 500;
+  cashBonus = 0;
+  cost2 = 500;
+  cost3 = 500;
+  bulletSize = 10;
+  cost4 = 1500;
+  enemySpeed = 1;
+  cost5 = 10000;
+  multishot = false;
+  cost6 = 50000;
+  secret = false;
 
   projectiles = [];
   enemies = [];
   scoreCounter.textContent = score;
+  cashCounter.textContent = cash;
+  speedCounter.textContent = bulletSpeed;
+  cost1Counter.textContent = cost1;
+  bonusCounter.textContent = "0%";
+  cost2Counter.textContent = cost2;
+  cost3Counter.textContent = cost3;
+  sizeCounter.textContent = bulletSize;
+  enemySpeedCounter.textContent = enemySpeed;
+  cost4Counter.textContent = cost4;
+  cost5Counter.textContent = cost5;
+  cost6Counter.textContent = cost6;
 
   loop()
 }
@@ -78,8 +111,8 @@ function update() {
   }
   //Enemy logic
   for (const enemy of enemies) {
-    enemy.x -= enemy.dx*enemy.speed;
-    enemy.y -= enemy.dy*enemy.speed;
+    enemy.x -= enemy.dx*enemySpeed;
+    enemy.y -= enemy.dy*enemySpeed;
     const xDist = enemy.x - turret.x;
     const yDist = enemy.y - turret.y;
     if (xDist*xDist+yDist*yDist < 85*85) {
@@ -102,11 +135,13 @@ function update() {
   for (let i = 0; i < enemies.length; i++) {
     if (enemies[i].dead) {
       score += 1;
+      cash += 100*(1+cashBonus/100);
       scoreCounter.textContent = score;
     };
   };
   //Enemy spawn
   time = performance.now();
+  nextWaveCounter.textContent = Math.floor((waveCd-time+prevWave)/100)/10;
   if (time - prevWave >= waveCd) {
     prevWave = time;
     spawnEnemy();
@@ -114,6 +149,8 @@ function update() {
   //Collision cleanup
   enemies = enemies.filter(e => !e.dead);
   projectiles = projectiles.filter(p => !p.dead);
+
+  cashCounter.textContent = cash;
 }
 
 function render() {
@@ -216,10 +253,10 @@ function spawnEnemy() {
       dead:false,
       dx:Math.cos(angle),
       dy:Math.sin(angle),
-      speed:1,
     });
   };
   waveSize += 1;
+  waveCd += 100;
 }
 
 canvas.addEventListener("mousemove", (e) => {
@@ -240,12 +277,131 @@ canvas.addEventListener("mousedown", (e) => {
     y:turret.y+50*dy,
     dx:dx,
     dy:dy,
-    speed:3,
-    radius:10,
+    speed:bulletSpeed,
+    radius:bulletSize,
     dead:false,
   });
+  if (multishot) {
+    setTimeout(() => {
+      projectiles.push({
+        x:turret.x+50*dx,
+        y:turret.y+50*dy,
+        dx:dx,
+        dy:dy,
+        speed:bulletSpeed,
+        radius:bulletSize,
+        dead:false,
+      });
+        setTimeout(() => {
+        projectiles.push({
+          x:turret.x+50*dx,
+          y:turret.y+50*dy,
+          dx:dx,
+          dy:dy,
+          speed:bulletSpeed,
+          radius:bulletSize,
+          dead:false,
+        });
+      },100);
+    },100);
+  }
+  if (secret) {
+    for (let i = 0; i < 20; i++) {
+      const idx = Math.cos(Math.PI*i/10)
+      const idy = Math.sin(Math.PI*i/10)
+      projectiles.push({
+        x:turret.x+50*dx,
+        y:turret.y+50*dy,
+        dx:idx,
+        dy:idy,
+        speed:bulletSpeed,
+        radius:bulletSize,
+        dead:false,
+  });
+    }
+  }
 });
 
 document.getElementById("restartBtn").addEventListener("click",initialize);
+
+document.getElementById("shop").addEventListener("click", (e) => {
+  if (!e.target.classList.contains("slot")) return;
+
+  switch (e.target.dataset.slot) {
+    case "1":
+      if (cash >= cost1) {
+        cash -= cost1
+        bulletSpeed ++;
+        speedCounter.textContent = bulletSpeed;
+        cost1 += 500;
+        cost1Counter.textContent = cost1;
+        if (bulletSpeed == 13) {
+          cost1 = 10000000000
+          cost1Counter.textContent = "Max";
+        }
+      }
+      break;
+    case "2":
+      if (cash >= cost2) {
+        cash -= cost2
+        cashBonus += 10;
+        bonusCounter.textContent = cashBonus+"%"
+        cost2 += 2000;
+        cost2Counter.textContent = cost2;
+        if (cashBonus == 100) {
+          cost2 = 10000000000
+          cost2Counter.textContent = "Max"
+        }
+      }
+      break;
+    case "3":
+      if (cash >= cost3) {
+        cash -= cost3;
+        bulletSize += 1;
+        sizeCounter.textContent = bulletSize;
+        cost3 += 500;
+        cost3Counter.textContent=cost3;
+        if (bulletSize == 30) {
+          cost3 = 10000000000;
+          cost3Counter.textContent = "Max"
+        }
+      }
+      break;
+    case "4":
+      if (cash >= cost4) {
+        cash -= cost4;
+        enemySpeed = Math.floor(enemySpeed * 0.95*1000)/1000
+        enemySpeedCounter.textContent = enemySpeed;
+        cost4 = Math.floor(cost4*1.5)
+        cost4Counter.textContent = cost4;
+        if (enemySpeed < 0.5) {
+          cost4 = 10000000000;
+          cost4Counter.textContent = "Max"
+        }
+
+      }
+      break;
+    case "5":
+      if (cash >= cost5) {
+        cash -= cost5;
+        multishot = true;
+        cost5 = 10000000000;
+        cost5Counter.textContent = "Already unlocked"
+      }
+      break;
+    case "6":
+      if (cash >= cost6) {
+        cash -= cost6;
+        cashBonus += 10000;
+        bonusCounter.textContent = cashBonus+"%"
+        secret = true;
+        cost6Counter.textContent = "Already unlocked"
+        cost6 = 10000000000;
+        
+      }
+      break;
+  }
+})
+
 
 initialize()
